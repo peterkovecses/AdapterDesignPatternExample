6dbef21c2f681453aecbeaaf08cd2edb6243e353
Péter Kövecses <kovecsespeter@gmail.com>
2022-03-26 09:23:47 +0100

    Add project files.

---
+++ AdapterDesignPatternExample/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace AdapterDesignPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The adapter is needed to add the Racecar object to this list.
+            List<IVehicle> vehicles = new List<IVehicle>();
+            vehicles.Add(new Car("Mercedes", 2000, 286));
+            vehicles.Add(new Car("Ford", 1400, 180));
+            vehicles.Add(new RacecarToVehicleAdapter(new Racecar("Porsche", 1390, 500)));
+
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine($"Weight power ratio: {vehicle.GetWeightPowerRatio()}");
+            }
+        }
+
+        // ITarget interface
+        public interface IVehicle
+        {
+            double GetWeightPowerRatio();
+        }
+
+        public class Car : IVehicle
+        {
+            public string Manufacturer { get; set; }
+            public int Weight { get; set; }
+            public int Power { get; set; }
+
+            public Car(string name, int weight, int power)
+            {
+                Manufacturer = name;
+                Weight = weight;
+                Power = power;
+            }
+
+            double IVehicle.GetWeightPowerRatio()
+            {
+                return ComputeService.ComputeWeightPowerRatio(Weight, Power);
+            }
+        }
+
+        // Adaptee class
+        public class Racecar
+        {
+            private int _weight;
+            private int _power;
+
+            public string Manufacturer { get; set; }
+            public int Weight
+            {
+                get
+                {
+                    return _weight;
+                }
+                set
+                {
+                    _weight = value;
+                    WeightPowerRatio = ComputeService.ComputeWeightPowerRatio(_weight, _power);
+                }
+            }
+
+            public int Power
+            {
+                get
+                {
+                    return _power;
+                }
+                set
+                {
+                    _power = value;
+                    WeightPowerRatio = ComputeService.ComputeWeightPowerRatio(_weight, _power);
+                }
+            }
+
+            public double WeightPowerRatio { get; private set; }
+
+            public Racecar(string name, int weight, int power)
+            {
+                Manufacturer = name;
+                Weight = weight;
+                Power = power;
+            }
+        }
+
+        // The adapter is derived from the class to be wrapped and implements the interface to be used.
+        public class RacecarToVehicleAdapter : Racecar, IVehicle
+        {
+            public RacecarToVehicleAdapter(Racecar racecar) : base(racecar.Manufacturer, racecar.Weight, racecar.Power)
+            {
+
+            }
+
+            double IVehicle.GetWeightPowerRatio()
+            {
+                return WeightPowerRatio;
+            }
+        }
+
+        public static class ComputeService
+        {
+            public static double ComputeWeightPowerRatio(int weight, int power)
+            {
+                if (power == 0)
+                    return 0;
+
+                return weight / power;
+            }
+        }
+    }
+}
